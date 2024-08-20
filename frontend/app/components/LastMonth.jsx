8aca6e4e999c2a32557efdc18d8bcf3973aa148c
ROMANVIKI <vikiadhi2016@gmail.com>
2024-08-20 22:25:14 +0530

    sep dirs

---
+++ frontend/app/components/LastMonth.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LastMonth = () => {
+  return (
+    <div>
+      <div><h2>Subtitles from last 30 days</h2></div>
+      <div></div>
+    </div>
+  )
+}
+
+export default LastMonth
---
+++ frontend/app/components/LatestSub.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const LatestSub = () => {
+  return (
+    <>
+      <div className="border-b border-b-white-950">
+        <div className="p-4 text-[#009900]">
+          <p className="text-2xl">Lates Subtitles</p>
+        </div>
+      </div>
+      <div>
+        what
+      </div>
+    </>
+  );
+};
+
+export default LatestSub;
